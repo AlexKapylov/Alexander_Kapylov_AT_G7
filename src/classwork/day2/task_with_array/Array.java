1accb640f3288d6ec65ffe0d8cb74afbccef4f37
Alex_Kapylov <alekskapylov2601@gmail.com>
2023-02-20 20:29:21 +0300

    i added classwork from second day

---
+++ src/classwork/day2/task_with_array/Array.java
@@ -0,0 +1,23 @@
+package classwork.day2.task_with_array;
+
+import java.util.Random;
+
+public class Array {
+    public void foo() {
+
+        int[] array = new int[10];
+        Random random = new Random();
+        double sum = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            array[i] = random.nextInt(100);
+//        }
+        for (int i : array) {
+            i = random.nextInt(100);
+            System.out.println(i);
+            sum += i;
+        }
+        double avg = sum / array.length;
+        System.out.println("Average value is: " + avg);
+
+    }
+}
---
+++ src/classwork/day2/task_with_array/Main.java
@@ -0,0 +1,8 @@
+package classwork.day2.task_with_array;
+
+public class Main {
+    public static void main(String[] args) {
+        Array array1 = new Array();
+        array1.foo();
+    }
+}
