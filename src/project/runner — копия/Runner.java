39e512f2e39e7fa3c17bea575ef0281e02db98c2
Alex_Kapylov <alekskapylov2601@gmail.com>
2023-02-20 20:44:07 +0300

    i tried to remove the uppercase symbols

---
+++ src/project/runner — копия/Runner.java
@@ -0,0 +1,16 @@
+package project.runner;
+
+import project.boxing.Bottle;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        Bottle myBottle1 = new Bottle(0.5, "o2");
+        Bottle myBottle2 = new Bottle(1, "co");
+        Bottle myBottle3 = new Bottle(1.5, "no");
+        myBottle1.open();
+        myBottle2.open();
+        myBottle3.open();
+    }
+
+}
